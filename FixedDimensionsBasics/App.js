e26f290bd177f7f5dee08b3dd7c8cfd54a359bb7
thuansystemit <thuansystem@gmail.com>
2019-01-13 23:59:06 +0700

    Update Flex Dimensions

--- FixedDimensionsBasics/App.js
+++ FixedDimensionsBasics/App.js
@@ -20,10 +20,13 @@ type Props = {};
 export default class App extends Component<Props> {
   render() {
     return (
-      <View>
-        <View style={{width: 50, height: 50, backgroundColor: 'powderblue'}} />
-        <View style={{width: 100, height: 100, backgroundColor: 'skyblue'}} />
-        <View style={{width: 150, height: 150, backgroundColor: 'steelblue'}} />
+      // Try removing the `flex: 1` on the parent View.
+      // The parent will not have dimensions, so the children can't expand.
+      // What if you add `height: 300` instead of `flex: 1`?
+      <View style={{flex: 1}}>
+        <View style={{flex: 1, backgroundColor: 'powderblue'}} />
+        <View style={{flex: 2, backgroundColor: 'skyblue'}} />
+        <View style={{flex: 3, backgroundColor: 'steelblue'}} />
       </View>
     );
   }
